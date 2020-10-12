bc189d6057096415e9f8be9b8992f63d914dcafb
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2020-10-12 00:24:15 -0300

    configurando login do usuario e sessoes

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php 
+session_start();
 require_once("vendor/autoload.php");
+require_once("functions.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
-use Hcode\PageAdmin;
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 $app->config('debug', true);
@@ -13,11 +16,31 @@ $app->get('/', function() {
 	$page->setTpl("index");
 });
 
-$app->get('/admin', function() {
+$app->get('/admin', function(){
+	User::verifyLogin();
 	$page = new PageAdmin();
 	$page->setTpl("index");
 });
 
-$app->run();
+$app->get('/admin/login', function(){
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	$page->setTpl("login");
+});
 
+$app->post('/admin/login', function(){
+	User::login(post('deslogin'), post('despassword'));
+	header("Location: /admin");
+	exit;
+});
+
+$app->get('/admin/logout', function(){
+	User::logout();
+	header("Location: /admin/login");
+	exit;
+});
+
+$app->run();
  ?>
