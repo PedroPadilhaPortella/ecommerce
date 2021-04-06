99eaacb8142b9655ea1d80d8095e171724bc60fa
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2021-04-06 08:33:22 -0300

    Update index.php

--- index.php
+++ index.php
@@ -46,34 +46,13 @@ $app->get('/admin/users', function() {
 	User::verifyLogin();
 	$users = User::listAll();
 	$page = new PageAdmin();
-	$page->setTpl("users", array(
-		"users"=>$users
-	));
+	$page->setTpl("users", array("users"=>$users));
 });
 
 $app->get('/admin/users/create', function() {
 	User::verifyLogin();
 	$page = new PageAdmin();
 	$page->setTpl("users-create");
-});
-
-$app->get('/admin/users/:iduser/delete', function($iduser) {
-	User::verifyLogin();
-	$user = new User();
-	$user->get((int)$iduser);
-	$user->delete();
-	header("Location: /admin/users");
-	exit;
-});
-
-$app->get('/admin/users/:iduser', function($iduser) {
-	User::verifyLogin();
-	$user = new User();
-	$user->get((int)$iduser);
-	$page = new PageAdmin();
-	$page->setTpl("users-update", array(
-		"user"=>$user->getValues()
-	));
 });
 
 $app->post('/admin/users/create', function() {
@@ -87,16 +66,34 @@ $app->post('/admin/users/create', function() {
 	exit;
 });
 
-$app->post('/admin/users/:iduser', function($iduser) {
-	User::verifyLogin();
-	$user = new User();
-	$user->get((int)$iduser);
-	$_POST["inadmin"] = (isset($_POST["inadmin"]))? 1 : 0;
-	$user->setData($_POST);
-	$user->update();
-	header("Location: /admin/users");
-	exit;
+$app->get('/admin/users/:iduser/delete', function($iduser) {
+    User::verifyLogin();
+    $user = new User();
+    $user->get((int)$iduser);
+    $user->delete();
+    header("Location: /admin/users");
+    exit;
 });
+
+$app->get('/admin/users/:iduser', function($iduser) {
+    User::verifyLogin();
+    $user = new User();
+    $user->get((int)$iduser);
+    $page = new PageAdmin();
+    $page->setTpl("users-update", array("user"=>$user->getValues()));
+});
+
+// $app->post('/admin/users/:iduser', function($iduser) {
+//     User::verifyLogin();
+// 	$user = new User();
+// 	$user->get((int)$iduser);
+// 	$_POST["inadmin"] = (isset($_POST["inadmin"]))? 1 : 0;
+// 	$user->setData($_POST);
+// 	$user->update();
+// 	header("Location: /admin/users");
+// 	exit;
+// });
+
 
 $app->run();
 
